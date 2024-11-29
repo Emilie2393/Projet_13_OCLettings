df1bdef82ea974e3bb4b4448e86b5060a91e59ba
Emily2393 <emilie.rich4rd@gmail.com>
2024-11-29 16:47:22 +0100

    App refactoring

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Profile
 
-# Create your views here.
+
+# Index page for profiles part
+def index(request):
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+
+# Profile page for each username
+def profile(request, username):
+    profile = Profile.objects.get(user__username=username)
+    context = {'profile': profile}
+    return render(request, 'profiles/profile.html', context)
