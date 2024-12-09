43104832676e279d8c73d75af5d0462e7304aca6
Emily2393 <emilie.rich4rd@gmail.com>
2024-12-09 11:27:03 +0100

    test issue

--- lettings/tests.py
+++ lettings/tests.py
@@ -1,3 +1,21 @@
-from django.test import TestCase
+import pytest
+from django.urls import reverse, resolve
+from lettings.models import Address, Letting
+ 
+def test_lettings_index_url():
+    path = reverse('lettings_index')
+    assert path == "/lettings/"
+    assert resolve(path).view_name == "lettings_index"
 
-# Create your tests here.
+
+@pytest.mark.django_db
+def test_letting_object_url():
+    address = Address.objects.create(number=5, street="rue du Test", city="Rennes", state="FR", zip_code=35000, country_iso_code="FR")
+    print(address)
+    Letting.objects.create(title="test", address=1)
+    path = reverse('letting', kwargs={'pk':1})
+    
+    assert path == "/1"
+    assert resolve(path).view_name == "letting"
+
+
--- oc_lettings_site/tests.py
+++ oc_lettings_site/tests.py
@@ -1,2 +1,18 @@
-def test_dummy():
-    assert 1
+from django.test import Client
+from django.urls import reverse, resolve
+from pytest_django.asserts import assertTemplateUsed
+ 
+def test_index_url():
+    path = reverse('index')
+    assert path == "/"
+    assert resolve(path).view_name == "index"
+
+def test_index_view():
+    client = Client()
+    response = client.get(reverse("index"))
+    content = "Welcome to Holiday Homes"
+    assert content in response.content.decode()
+    assert response.status_code == 200
+    assertTemplateUsed(response, "index.html")
+
+
