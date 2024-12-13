27350428a9cb6ec782700b01ee75e9f38233abda
Emily2393 <emilie.rich4rd@gmail.com>
2024-12-13 15:21:17 +0100

    testing

--- lettings/tests/tests.py
+++ lettings/tests/tests.py
@@ -1,5 +1,6 @@
 import pytest
 from django.urls import reverse, resolve
+from django.test import Client
 from lettings.models import Address, Letting
  
 def test_lettings_index_url():
@@ -11,11 +12,10 @@ def test_lettings_index_url():
 @pytest.mark.django_db
 def test_letting_object_url():
     address = Address.objects.create(number=5, street="rue du Test", city="Rennes", state="FR", zip_code=35000, country_iso_code="FR")
-    print(address)
-    Letting.objects.create(title="test", address=1)
-    path = reverse('letting', kwargs={'pk':1})
+    Letting.objects.create(title="test", address=address)
+    path = reverse('letting', args=[1])
     
-    assert path == "/1"
+    assert path == "/lettings/1/"
     assert resolve(path).view_name == "letting"
 
 
--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -21,15 +21,15 @@ ALLOWED_HOSTS = ["*"]
 # Application definition
 
 INSTALLED_APPS = [
-    'oc_lettings_site.apps.OCLettingsSiteConfig',
+    "oc_lettings_site.apps.OCLettingsSiteConfig",
+    "lettings.apps.LettingsConfig",
+    "profiles.apps.ProfilesConfig",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'lettings',
-    'profiles',
 ]
 
 MIDDLEWARE = [
