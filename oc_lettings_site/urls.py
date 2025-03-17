92a0b385bba129e7b995db598fa6597d25230a7f
Emily2393 <emilie.rich4rd@gmail.com>
2025-03-17 12:35:59 +0100

    404 bug

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from .views import custom_404
+from django.conf.urls import handler404
 
 from . import views
 
@@ -9,3 +11,5 @@ urlpatterns = [
     path('profiles/', include('profiles.urls')),
     path('admin/', admin.site.urls, name='admin'),
 ]
+
+handler404 = custom_404
--- oc_lettings_site/views.py
+++ oc_lettings_site/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 # Index page for Orange County Lettings website
 def index(request):
     return render(request, 'index.html')
+
+def custom_404(request, exception):
+    return render(request, '404.html', status=404)
